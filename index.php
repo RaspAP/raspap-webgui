ae11bfe7fc9d3305b4863f44295d476ffd252081
Bill Zimmerman <me@awesomebox.billz.to>
2015-02-25 15:18:46 +0100

    Fixed nav collapse css issue

--- index.php
+++ index.php
@@ -86,7 +86,8 @@ $page = $_GET['page'];
 <body>
 
 	<div id="wrapper">
-		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+		<!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 	                <span class="sr-only">Toggle navigation</span>
@@ -94,6 +95,7 @@ $page = $_GET['page'];
 	                <span class="icon-bar"></span>
 	                <span class="icon-bar"></span>
 	            </button>
+	            <a class="navbar-brand" href="index.html">Raspi Wifi Portal v1.0</a>
 			</div>
 			<!-- /.navbar-header -->
 
