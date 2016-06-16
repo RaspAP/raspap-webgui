b041bd6eda9af65b3b0d29af6a58a128aa70f080
Bill Zimmerman <me@awesomebox.billz.to>
2016-06-16 15:16:54 +0200

    Intial commit

--- installers/raspbian.sh
+++ installers/raspbian.sh
@@ -4,13 +4,13 @@ source /tmp/raspapcommon.sh && rm -f /tmp/raspapcommon.sh
 
 function update_system_packages() {
     install_log "Updating sources"
-    sudo apt-get update || install_error "Couldn't update package list"
-    sudo apt-get upgrade || install_error "Couldn't upgrade packages"
+    sudo apt-get update || install_error "Unable to update package list"
+    sudo apt-get upgrade || install_error "Unable to upgrade packages"
 }
 
 function install_dependencies() {
     install_log "Installing required packages"
-    sudo apt-get install lighttpd php5-cgi || install_error "Couldn't install dependencies"
+    sudo apt-get install lighttpd php5-cgi git || install_error "Unable to install dependencies"
 }
 
 install_raspap
