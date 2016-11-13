f0cf084202b93cc9b425424027817acdf9ee70e3
Joe Haig <josephhaig@gmail.com>
2016-11-13 17:42:50 +0000

    Add rc.local file to set up NAT routing

--- installers/common.sh
+++ installers/common.sh
@@ -114,6 +114,7 @@ function default_configuration() {
     sudo mv $webroot_dir/config/hostapd.conf /etc/hostapd/hostapd.conf || install_error "Unable to move hostapd configuration file"
     sudo mv $webroot_dir/config/dnsmasq.conf /etc/dnsmasq.conf || install_error "Unable to move dnsmasq configuration file"
     sudo mv $webroot_dir/config/dhcpcd.conf /etc/dhcpcd.conf || install_error "Unable to move dhcpcd configuration file"
+    sudo mv $webroot_dir/config/rc.local /etc/rc.local || install_error "Unable to move rc.local file"
 }
 
 # Add a single entry to the sudoers file
