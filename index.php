9a148e848b54e13fb40dc6c5a0267e06a6cc4cdb
Joe Haig <josephhaig@gmail.com>
2017-06-03 17:15:23 +0100

    Re-add X-UA-Compatible meta tag

--- index.php
+++ index.php
@@ -119,6 +119,7 @@ array_push($navigation,
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, inital-scale=1">
 
     <title>Raspbian WiFi Configuration Portal</title>
