18fa472d2c9d86dbbaf196ff0a807c19a1afbb32
Lawrence <sirlagz@gmail.com>
2017-10-03 12:32:30 +0800

    Fixed email address typo

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * php5-cgi (I have version 5.4.4-12 installed via apt)
  * along with their supporting packages, php5 will also need to be enabled.
  * 
- * @author     Lawrence Yau <sirlagz@gmail.comm>
+ * @author     Lawrence Yau <sirlagz@gmail.com>
  * @author     Bill Zimmerman <billzimmerman@gmail.com>
  * @license    GNU General Public License, version 3 (GPL-3.0)
  * @version    1.1
