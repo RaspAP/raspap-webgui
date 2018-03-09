217698889852f3db5276c0c9a5ffa7f0ab6bf252
billz <billzimmerman@gmail.com>
2018-03-09 10:08:52 +0000

    Updated with minified sb-admin-2.css

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ $theme_url = 'dist/css/' . $theme;
     <link href="dist/css/timeline.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="dist/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
     <link href="vendor/morrisjs/morris.css" rel="stylesheet">
