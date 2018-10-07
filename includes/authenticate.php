da03ca68419766ad9e0c79ecf3a3ad16737946fc
D9ping <D9ping@users.noreply.github.com>
2018-10-07 02:18:28 +0200

    Added login bruteforce protection.
    Uses APCu for temporary remembering the attacking ip address in memory between requests.
    
    Signed-off-by: D9ping <D9ping@users.noreply.github.com>

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -2,6 +2,30 @@
 $user = $_SERVER['PHP_AUTH_USER'];
 $pass = $_SERVER['PHP_AUTH_PW'];
 
+if (!filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP)) {
+    exit();
+}
+
+define('SECONDSBLOCKBRUTEFORCE', 20);
+define('MAXNUMFAILEDLOGINS', 3);
+if (extension_loaded('apcu')) {
+    if (apcu_exists('ipaddrfailedlogin')) {
+        $lastIpAddrFailedLogin = apcu_fetch('ipaddrfailedlogin');
+        if ($_SERVER['REMOTE_ADDR'] === $lastIpAddrFailedLogin) {
+            $numFailedLogins = apcu_fetch('numfailedlogins');
+            if ($numFailedLogins >= MAXNUMFAILEDLOGINS) {
+                // Bruteforce detected.
+                if ($numFailedLogins > MAXNUMFAILEDLOGINS) {
+                    exit();
+                }
+
+                apcu_inc('numfailedlogins');
+                exit(sprintf('Too many failed logins. Please wait at least %d seconds before trying to login again.', SECONDSBLOCKBRUTEFORCE));
+            }
+        }
+    }
+}
+
 $validated = ($user == $config['admin_user']) && password_verify($pass, $config['admin_pass']);
 
 if (!$validated) {
@@ -13,6 +37,14 @@ if (!$validated) {
     header('HTTP/1.0 401 Unauthorized');
   }
 
+  if (extension_loaded('apcu')) {
+    apcu_store('ipaddrfailedlogin', $_SERVER['REMOTE_ADDR'], SECONDSBLOCKBRUTEFORCE);
+    if (!apcu_exists('numfailedlogins')) {
+        apcu_add('numfailedlogins', 0, SECONDSBLOCKBRUTEFORCE);
+    }
+
+    apcu_inc('numfailedlogins');
+  }
+
   exit('Not authorized'.PHP_EOL);
 }
-
