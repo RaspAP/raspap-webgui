5e374f3b84450ec62b3ceefa40f0ed23e2e4c9dd
billz <billzimmerman@gmail.com>
2018-10-17 15:51:27 +0000

    Added locale support for Chinese simplified, thanks @BANKA2017

--- includes/locale.php
+++ includes/locale.php
@@ -35,6 +35,9 @@ if (empty($_SESSION['locale']) && strlen($_SERVER['HTTP_ACCEPT_LANGUAGE']) >= 2)
     case "nl":
       $locale = "nl_NL.UTF-8";
       break;
+    case "zh":
+      $locale = "zh_CN.UTF-8";
+      break;
     default:  
       $locale = "en_GB.UTF-8";
       break;
