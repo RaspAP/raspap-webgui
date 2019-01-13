7b604a99a7640c127966ef75ad1fcf3ec4084447
billz <billzimmerman@gmail.com>
2019-01-13 08:52:29 +0000

    Added support for Español, thanks @lafm1990

--- includes/locale.php
+++ includes/locale.php
@@ -44,6 +44,9 @@ if (empty($_SESSION['locale']) && strlen($_SERVER['HTTP_ACCEPT_LANGUAGE']) >= 2)
     case "ru":
       $locale = "ru_RU.UTF-8";
       break;
+    case "es":
+      $locale = "es_MX.UTF-8";
+      break;
     default:  
       $locale = "en_GB.UTF-8";
       break;
