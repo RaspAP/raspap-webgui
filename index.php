6b0373dee5449c3533dcded384257d600fec6bc4
billz <billzimmerman@gmail.com>
2019-03-06 10:43:36 +0000

    Added bootstrap-toggle css+js

--- index.php
+++ index.php
@@ -69,6 +69,9 @@ $theme_url = 'dist/css/'.htmlspecialchars($theme, ENT_QUOTES);
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Bootstrap Toggle CSS -->
+    <link href="vendor/bootstrap-toggle/css/bootstrap-toggle.min.css" rel="stylesheet">
+
     <!-- MetisMenu CSS -->
     <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
@@ -238,6 +241,9 @@ $extraFooterScripts = array();
 
     <!-- Bootstrap Core JavaScript -->
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Bootstrap Toggle JavaScript -->
+    <script src="vendor/bootstrap-toggle/js/bootstrap-toggle.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
     <script src="vendor/metisMenu/metisMenu.min.js"></script>
