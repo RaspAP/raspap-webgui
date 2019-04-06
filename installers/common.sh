c83f2883ce03826a346b7b11fddf8f1101aecede
billz <billzimmerman@gmail.com>
2019-04-06 08:36:36 +0100

    bugfix: added slashes to iptables rules

--- installers/common.sh
+++ installers/common.sh
@@ -205,7 +205,7 @@ function default_configuration() {
     lines=(
     'echo 1 > \/proc\/sys\/net\/ipv4\/ip_forward #RASPAP'
     'iptables -t nat -A POSTROUTING -j MASQUERADE #RASPAP'
-    'iptables -t nat -A POSTROUTING -s 192.168.50.0/24 ! -d 192.168.50.0/24 -j MASQUERADE #RASPAP'
+    'iptables -t nat -A POSTROUTING -s 192.168.50.0\/24 ! -d 192.168.50.0\/24 -j MASQUERADE #RASPAP'
     )
     
     for line in "${lines[@]}"; do
