dd63294a37ef469763dd9ef1b64cc17765b410fc
billz <billzimmerman@gmail.com>
2019-04-13 15:19:13 +0000

    Updated version number

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
  * Enables use of simple web interface rather than SSH to control wifi and hostapd on the Raspberry Pi.
  * Recommended distribution is Raspbian Server Edition. Specific instructions to install the supported software are
  * in the README and original post by @SirLagz. For a quick run through, the packages required for the WebGUI are:
- * lighttpd (I have version 1.4.31-2 installed via apt)
- * php5-cgi (I have version 5.4.4-12 installed via apt)
- * along with their supporting packages, php5 will also need to be enabled.
+ * lighttpd (I have version 1.4.45 installed via apt)
+ * php7-cgi (I have version 7.0.33-0+deb9u3 installed via apt)
+ * along with their supporting packages, php7 will also need to be enabled.
  *
  * @author     Lawrence Yau <sirlagz@gmail.com>
  * @author     Bill Zimmerman <billzimmerman@gmail.com>
  * @license    GNU General Public License, version 3 (GPL-3.0)
- * @version    1.3.1
+ * @version    1.5
  * @link       https://github.com/billz/raspap-webgui
  * @see        http://sirlagz.net/2013/02/08/raspap-webgui/
  */
