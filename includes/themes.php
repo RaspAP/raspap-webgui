2215b11c219b92a300967f21d8c49b244c8e18f5
glaszig <glaszig@gmail.com>
2019-08-18 23:35:58 +0100

    use template for themes page

--- includes/themes.php
+++ includes/themes.php
@@ -22,35 +22,5 @@ function DisplayThemeConfig()
             break;
     }
 
-?>
-  <div class="row">
-  <div class="col-lg-12">
-  <div class="panel panel-primary">
-  <div class="panel-heading"><i class="fa fa-wrench fa-fw"></i> <?php echo _("Change Theme"); ?></div>
-  <div class="panel-body">
-
-    <h4><?php echo _("Theme settings"); ?></h4>
-
-    <div class="row">
-      <div class="form-group col-md-6">
-        <label for="code"><?php echo _("Select a theme"); ?></label>
-          <select class="form-control" id="theme-select"><?php echo _("Select a Theme"); ?>
-            <option value="default" class="theme-link"<?php echo $cselected; ?>>RaspAP (default)</option>
-            <option value="hackernews" class="theme-link"<?php echo $hselected; ?>>HackerNews</option>
-            <option value="terminal" class="theme-link"<?php echo $tselected; ?>>Terminal</option>
-          </select>
-      </div>
-    </div>
-
-    <form action="?page=system_info" method="POST">
-      <?php echo CSRFTokenFieldTag() ?>
-      <input type="button" class="btn btn-outline btn-primary" value="<?php echo _("Refresh"); ?>" onclick="document.location.reload(true)" />
-    </form>
-
-  </div><!-- /.panel-body -->
-  </div><!-- /.panel-primary -->
-  </div><!-- /.col-lg-12 -->
-  </div><!-- /.row -->
-<?php
+    echo renderTemplate("themes", compact("cselected", "hselected", "tselected"));
 }
-
