48c9880a92d8dcf0c3363505636d8994a55ab718
billz <billzimmerman@gmail.com>
2019-08-27 16:40:57 +0100

    Update panel class fix

--- templates/networking.php
+++ templates/networking.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-lg-12">
    <div class="panel panel-primary">
-    <div class="panel panel-heading"><i class="fa fa-sitemap fa-fw"></i> <?php echo _("Configure networking"); ?></div>
+    <div class="panel-heading"><i class="fa fa-sitemap fa-fw"></i> <?php echo _("Configure networking"); ?></div>
       <div class="panel-body">
         <div id="msgNetworking"></div>
         <ul class="nav nav-tabs">
