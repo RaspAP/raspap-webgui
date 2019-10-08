d1558413d4635163078767ec1d4a1b67b12cf4ca
billz <billzimmerman@gmail.com>
2019-10-08 18:36:31 +0100

    Update paths to reflect gulp build

--- includes/data_usage.php
+++ includes/data_usage.php
@@ -8,9 +8,8 @@ function DisplayDataUsage(&$extraFooterScripts)
     exec("ip -o link show | awk -F ': ' '{print $2}' | grep -v lo ", $interfacesWlo);
     echo renderTemplate("data_usage", [ "interfaces" => $interfacesWlo ]);
 
-    $extraFooterScripts[] = array('src'=>'vendor/raphael/raphael.min.js',
-                                  'defer'=>false);
-    $extraFooterScripts[] = array('src'=>'vendor/morrisjs/morris.min.js', 'defer'=>false);
-    $extraFooterScripts[] = array('src'=>'vendor/datatables/js/jquery.dataTables.min.js', 'defer'=>false);
-    $extraFooterScripts[] = array('src'=>'js/bandwidthcharts.js', 'defer'=>false);
+    $extraFooterScripts[] = array('src'=>'dist/raphael/raphael.min.js', 'defer'=>false);
+    $extraFooterScripts[] = array('src'=>'dist/morrisjs/morris.min.js', 'defer'=>false);
+    $extraFooterScripts[] = array('src'=>'dist/datatables/jquery.dataTables.min.js', 'defer'=>false);
+    $extraFooterScripts[] = array('src'=>'app/js/bandwidthcharts.js', 'defer'=>false);
 }
