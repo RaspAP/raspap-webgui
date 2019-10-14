73914b9c084c50508336652f709f842b9a4715cb
billz <billzimmerman@gmail.com>
2019-10-14 21:56:45 +0100

    Update fontawesome class

--- templates/themes.php
+++ templates/themes.php
@@ -3,7 +3,6 @@
     <div class="card">
       <div class="card-header"><i class="fas fa-paint-brush mr-2"></i><?php echo _("Change Theme"); ?></div>
       <div class="card-body">
-
         <h4><?php echo _("Theme settings"); ?></h4>
         <div class="row">
           <div class="form-group col-md-6">
@@ -11,12 +10,10 @@
             <?php SelectorOptions("theme", $themes, $selectedTheme, "theme-select") ?>
           </div>
         </div>
-
         <form action="?page=system_info" method="POST">
             <?php echo CSRFTokenFieldTag() ?>
-          <a href="?page=<?php echo $_GET['page'] ?>" class="btn btn-outline btn-primary"><i class="fa fa-refresh"></i> <?php echo _("Refresh"); ?></a>
+          <a href="?page=<?php echo $_GET['page'] ?>" class="btn btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh"); ?></a>
         </form>
-
       </div><!-- /.card-body -->
       <div class="card-footer"></div>
     </div><!-- /.card -->
