457347836cee13570d30e1167187d2eb57a6143d
billz <billzimmerman@gmail.com>
2019-10-15 13:45:35 +0100

    Adjust layout, apply bootstrap4 classes

--- templates/configure_client.php
+++ templates/configure_client.php
@@ -2,13 +2,16 @@
   <div class="col-lg-12">
     <div class="card">
       <div class="card-header"><i class="fas fa-wifi mr-2"></i><?php echo _("Configure WiFi client"); ?></div>
-      <!-- /.card-heading -->
+      <!-- /.card-header -->
       <div class="card-body">
         <?php $status->showMessages(); ?>
-        <h4><?php echo _("Client settings"); ?></h4>
-
-        <div class="btn-group btn-block">
-          <button type="button" style="padding:10px;float: right;display: block;position: relative;margin-top: -55px;" class="col-md-2 btn btn-info js-reload-wifi-stations"><?php echo _("Rescan"); ?></button>
+        <div class="row">
+          <div class="col">
+            <h4 class="mb-2"><?php echo _("Client settings"); ?></h4>
+          </div>
+          <div class="col-xs mr-2 mb-3">
+            <button type="button" class="btn btn-info btn-block float-right js-reload-wifi-stations"><?php echo _("Rescan"); ?></button>
+          </div>
         </div>
         <form method="POST" action="?page=wpa_conf" name="wpa_conf_form" class="row">
             <?php echo CSRFTokenFieldTag() ?>
