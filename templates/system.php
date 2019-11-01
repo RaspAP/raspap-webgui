aea38439ef16af5a6a6a3b7cce44a45743ecf5ce
billz <billzimmerman@gmail.com>
2019-11-01 16:02:22 +0000

    Minor: fix indents

--- templates/system.php
+++ templates/system.php
@@ -50,48 +50,49 @@ if ($cpuload > 90) {
 					<div role="tabpanel" class="tab-pane active" id="system">
 						<div class="row">
 							<div class="col-lg-6">
-					<h4 class="mt-3"><?php echo _("System Information"); ?></h4>
-					<div class="info-item"><?php echo _("Hostname"); ?></div><div><?php echo htmlspecialchars($hostname, ENT_QUOTES); ?></div>
-					<div class="info-item"><?php echo _("Pi Revision"); ?></div><div><?php echo htmlspecialchars(RPiVersion(), ENT_QUOTES); ?></div>
-					<div class="info-item"><?php echo _("Uptime"); ?></div><div><?php echo htmlspecialchars($uptime, ENT_QUOTES); ?></div>
-					<div class="mb-1"><?php echo _("Memory Used"); ?></div>
-					<div class="progress mb-2" style="height: 20px;">
-						<div class="progress-bar bg-<?php echo htmlspecialchars($memused_status, ENT_QUOTES); ?>"
-													role="progressbar" aria-valuenow="<?php echo htmlspecialchars($memused, ENT_QUOTES); ?>" aria-valuemin="0" aria-valuemax="100"
-													style="width: <?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%"><?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%
-												</div>
-					</div>
-					<div class="mb-1"><?php echo _("CPU Load"); ?></div>
-					<div class="progress mb-4" style="height: 20px;">
-						<div class="progress-bar bg-<?php echo htmlspecialchars($cpuload_status, ENT_QUOTES); ?>"
-													role="progressbar" aria-valuenow="<?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>" aria-valuemin="0" aria-valuemax="100"
-													style="width: <?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%"><?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%
-						</div>
-					</div>
+								<h4 class="mt-3"><?php echo _("System Information"); ?></h4>
+								<div class="info-item"><?php echo _("Hostname"); ?></div><div><?php echo htmlspecialchars($hostname, ENT_QUOTES); ?></div>
+								<div class="info-item"><?php echo _("Pi Revision"); ?></div><div><?php echo htmlspecialchars(RPiVersion(), ENT_QUOTES); ?></div>
+								<div class="info-item"><?php echo _("Uptime"); ?></div><div><?php echo htmlspecialchars($uptime, ENT_QUOTES); ?></div>
+								<div class="mb-1"><?php echo _("Memory Used"); ?></div>
+								<div class="progress mb-2" style="height: 20px;">
+									<div class="progress-bar bg-<?php echo htmlspecialchars($memused_status, ENT_QUOTES); ?>"
+																role="progressbar" aria-valuenow="<?php echo htmlspecialchars($memused, ENT_QUOTES); ?>" aria-valuemin="0" aria-valuemax="100"
+																style="width: <?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%"><?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%
+									</div>
+								</div>
+								<div class="mb-1"><?php echo _("CPU Load"); ?></div>
+								<div class="progress mb-4" style="height: 20px;">
+									<div class="progress-bar bg-<?php echo htmlspecialchars($cpuload_status, ENT_QUOTES); ?>"
+																role="progressbar" aria-valuenow="<?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>" aria-valuemin="0" aria-valuemax="100"
+																style="width: <?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%"><?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%
+									</div>
+								</div>
 
-					<form action="?page=system_info" method="POST">
-								<?php echo CSRFTokenFieldTag() ?>
-					<?php if (!RASPI_MONITOR_ENABLED) : ?>
-							<input type="submit" class="btn btn-warning" name="system_reboot"   value="<?php echo _("Reboot"); ?>" />
-							<input type="submit" class="btn btn-warning" name="system_shutdown" value="<?php echo _("Shutdown"); ?>" />
-					<?php endif ?>
-											<a href="?page=<?php echo $_GET['page'] ?>" class="btn btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh") ?></a>
-					</form>
+								<form action="?page=system_info" method="POST">
+									<?php echo CSRFTokenFieldTag() ?>
+									<?php if (!RASPI_MONITOR_ENABLED) : ?>
+										<input type="submit" class="btn btn-warning" name="system_reboot"   value="<?php echo _("Reboot"); ?>" />
+										<input type="submit" class="btn btn-warning" name="system_shutdown" value="<?php echo _("Shutdown"); ?>" />
+									<?php endif ?>
+										<a href="?page=<?php echo $_GET['page'] ?>" class="btn btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh") ?></a>
+								</form>
+								</div>
 							</div>
 						</div>
-					</div>
 
 					<div role="tabpanel" class="tab-pane" id="language">
 						<h4 class="mt-3"><?php echo _("Language settings") ;?></h4>
 						<div class="row">
 							<div class="form-group col-md-6">
-					<label for="code"><?php echo _("Select a language"); ?></label>
-							<?php SelectorOptions('locale', $arrLocales, $_SESSION['locale']); ?>
+								<label for="code"><?php echo _("Select a language"); ?></label>
+								<?php SelectorOptions('locale', $arrLocales, $_SESSION['locale']); ?>
 							</div>
 						</div>
 						<input type="submit" class="btn btn-outline btn-primary" name="SaveLanguage" value="<?php echo _("Save settings"); ?>" />
 						<a href="?page=<?php echo $_GET['page'] ?>" class="btn btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh") ?></a>
 					</div>
+
 					<div role="tabpanel" class="tab-pane" id="console">
 						<div class="row">
 							<div class="col-lg-12 mt-3">
