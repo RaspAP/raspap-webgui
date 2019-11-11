24b669b157403cf52a3c90ea7371be7283b11409
billz <billzimmerman@gmail.com>
2019-11-11 13:37:06 +0000

    Added -r option, changed msgs

--- installers/configport.sh
+++ installers/configport.sh
@@ -6,11 +6,29 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 server_port=$1
 lighttpd_conf=$2
 host=$3
+restart_service=0
 
-echo "Changing lighttpd server.port to $server_port..."
-sed -i "s/^\(server\.port *= *\)[0-9]*/\1$server_port/g" "$lighttpd_conf"
+while :; do
+    case $1 in
+        -r|--restart)
+        restart_service=1
+        shift
+        ;;
+        *)
+        break
+        ;;
+    esac
+done
 
-echo "RaspAP will now be available at $host:$server_port"
-echo "Restarting lighttpd in 5 seconds..."
-sleep 5
-systemctl restart lighttpd.service
+if [ "$restart_service" = 1 ]; then
+    echo "Restarting lighttpd in 3 seconds..."
+    sleep 3
+    systemctl restart lighttpd.service
+else
+    echo "Changing lighttpd server.port to $server_port..."
+    sed -i "s/^\(server\.port *= *\)[0-9]*/\1$server_port/g" "$lighttpd_conf"
+
+    echo "RaspAP will now be available at $host:$server_port"
+    echo "Restart lighttpd for new setting to take effect"
+fi
+
