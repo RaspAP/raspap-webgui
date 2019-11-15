93e3af0ecd6192378ff348101f7a3c3281732c25
billz <billzimmerman@gmail.com>
2019-11-15 08:45:54 +0000

    Update w/ openvpn service status indicator

--- templates/openvpn.php
+++ templates/openvpn.php
@@ -1,8 +1,23 @@
   <div class="row">
     <div class="col-lg-12">
       <div class="card">
-      <div class="card-header"><i class="fas fa-key fa-fw mr-2"></i>Configure OpenVPN</div>
+        <div class="card-header">
+          <div class="row">
+            <div class="col">
+              <i class="fas fa-key fa-fw mr-2"></i><?php echo _("Configure OpenVPN"); ?>
+            </div>
+            <div class="col">
+              <button class="btn btn-light btn-icon-split btn-sm service-status float-right">
+                <span class="icon text-gray-600"><i class="fas fa-circle service-status-<?php echo $serviceStatus ?>"></i></span>
+                <span class="text service-status">openvpn <?php echo _($serviceStatus) ?></span>
+              </button>
+            </div>
+          </div><!-- /.row -->
+        </div><!-- /.card-header -->
         <div class="card-body">
+        <?php $status->showMessages(); ?>
+          <form role="form" action="?page=openvpn_conf" enctype="multipart/form-data" method="POST">
+            <?php echo CSRFTokenFieldTag() ?>
             <!-- Nav tabs -->
             <ul class="nav nav-tabs">
                 <li class="nav-item"><a class="nav-link active" id="clienttab" href="#openvpnclient" data-toggle="tab"><?php echo _("Client settings"); ?></a></li>
@@ -10,53 +25,50 @@
             </ul>
             <!-- Tab panes -->
             <div class="tab-content">
-                <p><?php echo $status; ?></p>
-                <div class="tab-pane active" id="openvpnclient">
-                    <h4>Client settings</h4>
-                    <form role="form" action="?page=save_hostapd_conf" method="POST">
-                    <?php echo CSRFTokenFieldTag() ?>
-
-                    <div class="row">
-					  <div class="form-group col-md-6">
-						  <label for="code"><?php echo _("Username"); ?></label>
-						  <input type="text" class="form-control" name="Username" value="<?php echo htmlspecialchars($Username, ENT_QUOTES); ?>" />
-					  </div>
-					</div>
-                    <div class="row">
-					  <div class="form-group col-md-6">
-						  <label for="code"><?php echo _("Password"); ?></label>
-						  <input type="password" class="form-control" name="Password" value="<?php echo htmlspecialchars($Password, ENT_QUOTES); ?>" />
-					  </div>
-					</div>
-                    <div class="row">
-                        <div class="form-group col-md-6">
-                          <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="customFile">
-                            <label class="custom-file-label" for="customFile">Select OpenVPN configuration file (.ovpn)</label>
-                          </div>
-                        </div>
+              <div class="tab-pane active" id="openvpnclient">
+                <h4 class="mt-3"><?php echo _("Client settings"); ?></h4>
+                  <div class="row">
+                   <div class="form-group col-md-6">
+                    <label for="code"><?php echo _("Username"); ?></label>
+                      <input type="text" class="form-control" name="authUser" value="<?php echo htmlspecialchars($authUser, ENT_QUOTES); ?>" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="form-group col-md-6">
+                      <label for="code"><?php echo _("Password"); ?></label>
+                      <input type="password" class="form-control" name="authPassword" value="<?php echo htmlspecialchars($authPassword, ENT_QUOTES); ?>" />
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="form-group col-md-6">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" name="customFile" id="customFile">
+                        <label class="custom-file-label" for="customFile"><?php echo _("Select OpenVPN configuration file (.ovpn)"); ?></label>
+                      </div>
                     </div>
                 </div>
-                <div class="tab-pane fade" id="openvpnlogoutput">
-				  <h4><?php echo _("Client log"); ?></h4>
-				  <div class="row">
-					<div class="form-group col-md-8">
-					  <?php
-						  $log = file_get_contents('/tmp/hostapd.log');
-						  echo '<br /><textarea class="logoutput">'.htmlspecialchars($log, ENT_QUOTES).'</textarea>';
-					  ?>
-					</div>
-				  </div>
+              </div>
+              <div class="tab-pane fade" id="openvpnlogoutput">
+                <h4 class="mt-3"><?php echo _("Client log"); ?></h4>
+                <div class="row">
+                  <div class="form-group col-md-8">
+                    <?php
+                        $log = file_get_contents('/tmp/openvpn.log');
+                        echo '<textarea class="logoutput">'.htmlspecialchars($log, ENT_QUOTES).'</textarea>';
+                    ?>
+                  </div>
                 </div>
-                <input type="submit" class="btn btn-outline btn-primary" name="SaveOpenVPNSettings" value="Save settings" />
-                <?php
-                if ($hostapdstatus[0] == 0) {
-                    echo '<input type="submit" class="btn btn-success" name="StartOpenVPN" value="Start OpenVPN" />' , PHP_EOL;
-                } else {
+              </div>
+              <?php if (!RASPI_MONITOR_ENABLED) : ?>
+                  <input type="submit" class="btn btn-outline btn-primary" name="SaveOpenVPNSettings" value="Save settings" />
+                  <?php if ($openvpnstatus[0] == 0) {
+					  echo '<input type="submit" class="btn btn-success" name="StartOpenVPN" value="Start OpenVPN" />' , PHP_EOL;
+				  } else {
                     echo '<input type="submit" class="btn btn-warning" name="StopOpenVPN" value="Stop OpenVPN" />' , PHP_EOL;
-                }
-                ?>
-                </form>
+                  }
+                  ?>
+              <?php endif ?>
+              </form>
             </div>
         </div><!-- /.card-body -->
     <div class="card-footer"> Information provided by openvpn</div>
