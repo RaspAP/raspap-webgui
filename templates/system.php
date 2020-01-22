303347ac00a73676ab7ff22f02d8e8d0daed0c5c
Weylin-fr <bernard.charly@gmx.com>
2020-01-22 10:59:02 +0100

    Update system.php
    removed "</div>" as billz request it in the master branch

--- templates/system.php
+++ templates/system.php
@@ -79,7 +79,6 @@ if ($cputemp > 70) {
                                               style="width: <?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%"><?php echo htmlspecialchars($cpuload, ENT_QUOTES); ?>%
                   </div>
                 </div>
-                </div>
                 <div class="mb-1"><?php echo _("CPU Temp"); ?></div>
                 <div class="progress mb-4" style="height: 20px;">
                   <div class="progress-bar bg-<?php echo htmlspecialchars($cputemp_status, ENT_QUOTES); ?>"
