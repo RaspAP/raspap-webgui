c3c15d49adc397eb2bffaaac50b9515f32b53309
billz <billzimmerman@gmail.com>
2020-02-02 11:35:10 +0000

    Updated w/ include sysstats

--- templates/system.php
+++ templates/system.php
@@ -1,39 +1,6 @@
 <?php
 
-$hostname = $system->hostname();
-$uptime   = $system->uptime();
-$cores    = $system->processorCount();
-
-// mem used
-$memused  = $system->usedMemory();
-$memused_status = "primary";
-if ($memused > 90) {
-  $memused_status = "danger";
-} elseif ($memused > 75) {
-  $memused_status = "warning";
-} elseif ($memused >  0) {
-  $memused_status = "success";
-}
-
-// cpu load
-$cpuload = $system->systemLoadPercentage();
-if ($cpuload > 90) {
-  $cpuload_status = "danger";
-} elseif ($cpuload > 75) {
-  $cpuload_status = "warning";
-} elseif ($cpuload >  0) {
-  $cpuload_status = "success";
-}
-
-// cpu temp
-$cputemp = $system->systemTemperature();
-if ($cputemp > 70) {
-  $cputemp_status = "danger";
-} elseif ($cputemp > 50) {
-  $cputemp_status = "warning";
-} else {
-  $cputemp_status = "success";
-}
+include_once('includes/sysstats.php');
 
 ?>
 <div class="row">
