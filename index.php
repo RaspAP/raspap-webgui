3efce777b53b081f22bb17bb687393f6fa60b0ab
billz <billzimmerman@gmail.com>
2020-02-03 09:33:23 +0000

    Update widget LED values from sysstat

--- index.php
+++ index.php
@@ -119,9 +119,9 @@ if ($_COOKIE['sidebarToggled'] == 'true' ) {
           </div>
           <div class="col-xs ml-2">
             <div class="ml-1">Status</div>
-            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle <?php echo htmlspecialchars($hostapd_led, ENT_QUOTES); ?>"></i></span> Hotspot <?php echo _($hostapd_status); ?></div>
-            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle service-status-up"></i></span> Memory Use: <?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%</div>
-            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle service-status-up"></i></span> CPU Temp: <?php echo htmlspecialchars($cputemp, ENT_QUOTES); ?>°C</div>
+            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle <?php echo ($hostapd_led); ?>"></i></span> Hotspot <?php echo _($hostapd_status); ?></div>
+            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle <?php echo ($memused_led); ?>"></i></i></span> Memory Use: <?php echo htmlspecialchars($memused, ENT_QUOTES); ?>%</div>
+            <div class="info-item-xs"><span class="icon"><i class="fas fa-circle <?php echo ($cputemp_led); ?>"></i></span> CPU Temp: <?php echo htmlspecialchars($cputemp, ENT_QUOTES); ?>°C</div>
           </div>
         </div>
 	<li class="nav-item">
