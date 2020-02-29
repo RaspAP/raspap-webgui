490ca1cb5ad84cce56c2975784b505c1d25eec31
glaszig <glaszig@gmail.com>
2020-02-29 19:19:15 +0100

    added "advanced" tab to dhcp config, moved upstream dns into it

--- templates/dhcp.php
+++ templates/dhcp.php
@@ -21,6 +21,7 @@
     <!-- Nav tabs -->
     <ul class="nav nav-tabs">
       <li class="nav-item"><a class="nav-link active" href="#server-settings" data-toggle="tab"><?php echo _("Server settings"); ?></a></li>
+      <li class="nav-item"><a class="nav-link" href="#advanced" data-toggle="tab"><?php echo _("Advanced settings"); ?></a></li>
       <li class="nav-item"><a class="nav-link" href="#static-leases" data-toggle="tab"><?php echo _("Static Leases") ?></a></li>
       <li class="nav-item"><a class="nav-link" href="#client-list" data-toggle="tab"><?php echo _("Client list"); ?></a></li>
     </ul>
@@ -84,61 +85,6 @@
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-md-6">
-        <h5><?php echo _("Upstream DNS servers") ?></h5>
-
-        <div class="input-group">
-          <input type="hidden" name="no-resolv" value="0">
-          <div class="custom-control custom-switch">
-            <input class="custom-control-input" id="no-resolv" type="checkbox" name="no-resolv" value="1" <?php echo $conf['no-resolv'] ? ' checked="checked"' : "" ?> aria-describedby="no-resolv-description">
-            <label class="custom-control-label" for="no-resolv"><?php echo _("Only ever query DNS servers configured below") ?></label>
-          </div>
-          <p id="no-resolv-description">
-            <small><?php echo _("Enable this option if you want RaspAP to <b>send DNS queries to the servers configured below exclusively</b>. By default RaspAP also uses its upstream DHCP server's name servers.") ?></small>
-            <br><small class="text-muted"><?php echo _("This option adds <code>no-resolv</code> to the dnsmasq configuration.") ?></small>
-          </p>
-        </div>
-
-        <div class="js-dhcp-upstream-servers">
-          <?php foreach ($upstreamServers as $server): ?>
-            <div class="form-group input-group input-group-sm js-dhcp-upstream-server">
-              <input type="text" class="form-control" name="server[]" value="<?php echo $server ?>">
-              <div class="input-group-append">
-                <button class="btn btn-outline-secondary js-remove-dhcp-upstream-server" type="button"><i class="fas fa-minus"></i></button>
-              </div>
-            </div>
-          <?php endforeach ?>
-        </div>
-
-        <div class="form-group">
-          <label for="add-dhcp-upstream-server-field"><?php echo _("Add upstream DNS server") ?></label>
-          <div class="input-group">
-            <input type="text" class="form-control" id="add-dhcp-upstream-server-field" aria-describedby="new-dhcp-upstream-server" placeholder="<?php printf(_("e.g. %s"), "208.67.222.222") ?>">
-            <div class="input-group-append">
-              <button type="button" class="btn btn-outline-secondary js-add-dhcp-upstream-server"><i class="fas fa-plus"></i></button>
-            </div>
-          </div>
-          <p id="new-dhcp-upstream-server" class="form-text text-muted">
-            <small>
-              <?php echo _("Format: ") ?>
-              <code class="text-muted"><?php echo htmlspecialchars("[/[<domain>]/[domain/]][<ipaddr>[#<port>][@<source-ip>|<interface>[#<port>]]"); ?></code>
-            </small>
-          </p>
-        </div>
-      </div>
-    </div>
-
-    <template id="dhcp-upstream-server">
-      <div class="form-group input-group input-group-sm js-dhcp-upstream-server">
-        <input type="text" class="form-control" name="server[]" value="{{ server }}">
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary js-remove-dhcp-upstream-server" type="button"><i class="fas fa-minus"></i></button>
-        </div>
-      </div>
-    </template>
-
-
     <?php if (!RASPI_MONITOR_ENABLED) : ?>
         <input type="submit" class="btn btn-outline btn-primary" value="<?php echo _("Save settings"); ?>" name="savedhcpdsettings" />
         <?php if ($dnsmasq_state) : ?>
@@ -148,6 +94,66 @@
         <?php endif ?>
     <?php endif ?>
     </div><!-- /.tab-pane -->
+
+
+    <!-- advanced tab -->
+    <div class="tab-pane" id="advanced">
+
+      <div class="row">
+        <div class="col-md-6">
+          <h5><?php echo _("Upstream DNS servers") ?></h5>
+
+          <div class="input-group">
+            <input type="hidden" name="no-resolv" value="0">
+            <div class="custom-control custom-switch">
+              <input class="custom-control-input" id="no-resolv" type="checkbox" name="no-resolv" value="1" <?php echo $conf['no-resolv'] ? ' checked="checked"' : "" ?> aria-describedby="no-resolv-description">
+              <label class="custom-control-label" for="no-resolv"><?php echo _("Only ever query DNS servers configured below") ?></label>
+            </div>
+            <p id="no-resolv-description">
+              <small><?php echo _("Enable this option if you want RaspAP to <b>send DNS queries to the servers configured below exclusively</b>. By default RaspAP also uses its upstream DHCP server's name servers.") ?></small>
+              <br><small class="text-muted"><?php echo _("This option adds <code>no-resolv</code> to the dnsmasq configuration.") ?></small>
+            </p>
+          </div>
+
+          <div class="js-dhcp-upstream-servers">
+            <?php foreach ($upstreamServers as $server): ?>
+              <div class="form-group input-group input-group-sm js-dhcp-upstream-server">
+                <input type="text" class="form-control" name="server[]" value="<?php echo $server ?>">
+                <div class="input-group-append">
+                  <button class="btn btn-outline-secondary js-remove-dhcp-upstream-server" type="button"><i class="fas fa-minus"></i></button>
+                </div>
+              </div>
+            <?php endforeach ?>
+          </div>
+
+          <div class="form-group">
+            <label for="add-dhcp-upstream-server-field"><?php echo _("Add upstream DNS server") ?></label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="add-dhcp-upstream-server-field" aria-describedby="new-dhcp-upstream-server" placeholder="<?php printf(_("e.g. %s"), "208.67.222.222") ?>">
+              <div class="input-group-append">
+                <button type="button" class="btn btn-outline-secondary js-add-dhcp-upstream-server"><i class="fas fa-plus"></i></button>
+              </div>
+            </div>
+            <p id="new-dhcp-upstream-server" class="form-text text-muted">
+              <small>
+                <?php echo _("Format: ") ?>
+                <code class="text-muted"><?php echo htmlspecialchars("[/[<domain>]/[domain/]][<ipaddr>[#<port>][@<source-ip>|<interface>[#<port>]]"); ?></code>
+              </small>
+            </p>
+          </div>
+        </div>
+
+        <template id="dhcp-upstream-server">
+          <div class="form-group input-group input-group-sm js-dhcp-upstream-server">
+            <input type="text" class="form-control" name="server[]" value="{{ server }}">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary js-remove-dhcp-upstream-server" type="button"><i class="fas fa-minus"></i></button>
+            </div>
+          </div>
+        </template>
+      </div><!-- /.row -->
+
+    </div><!-- /.tab-pane | advanded tab -->
 
 		<div class="tab-pane fade" id="client-list">
 		<h4 class="mt-3 mb-3">Client list</h4>
