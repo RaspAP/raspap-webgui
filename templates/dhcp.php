a45e3a5e324d1d76294778c0c4432e433eced4cb
glaszig <glaszig@gmail.com>
2020-02-29 19:22:44 +0100

    fixed nav-tabs margin to content below

--- templates/dhcp.php
+++ templates/dhcp.php
@@ -19,7 +19,7 @@
     <form method="POST" action="?page=dhcpd_conf" class="js-dhcp-settings-form">
     <?php echo CSRFTokenFieldTag() ?>
     <!-- Nav tabs -->
-    <ul class="nav nav-tabs">
+    <ul class="nav nav-tabs mb-4">
       <li class="nav-item"><a class="nav-link active" href="#server-settings" data-toggle="tab"><?php echo _("Server settings"); ?></a></li>
       <li class="nav-item"><a class="nav-link" href="#advanced" data-toggle="tab"><?php echo _("Advanced settings"); ?></a></li>
       <li class="nav-item"><a class="nav-link" href="#static-leases" data-toggle="tab"><?php echo _("Static Leases") ?></a></li>
