cff02c86fbf6bc6110ff0afd14e0ae6f5f54b397
billz <billzimmerman@gmail.com>
2020-03-05 18:34:21 +0000

    Update rc.local to set log ownership. Resolves #518

--- installers/common.sh
+++ installers/common.sh
@@ -252,6 +252,8 @@ function default_configuration() {
     'echo 1 > \/proc\/sys\/net\/ipv4\/ip_forward #RASPAP'
     'iptables -t nat -A POSTROUTING -j MASQUERADE #RASPAP'
     'iptables -t nat -A POSTROUTING -s 192.168.50.0\/24 ! -d 192.168.50.0\/24 -j MASQUERADE #RASPAP'
+    'chown dnsmasq:www-data /tmp/dnsmasq.log #RASPAP'
+    'chown root:www-data /tmp/hostapd.log #RASPAP'
     )
     
     for line in "${lines[@]}"; do
