ab681689514f1433ea32fd8987dfec612406bfe4
billz <billzimmerman@gmail.com>
2020-03-27 14:31:50 +0000

    Update blocklist_dest

--- ajax/adblock/update_blocklist.php
+++ ajax/adblock/update_blocklist.php
@@ -6,7 +6,7 @@ require_once '../../includes/config.php';
 if (isset($_POST['blocklist_id'])) {
     $blocklist_id = $_POST['blocklist_id'];
     $notracking_url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/";
-    $dnsmasq_config = "/etc/dnsmasq.d/";
+    $blocklist_dest = "/etc/raspap/adblock/";
 
     switch ($blocklist_id) {
         case "notracking-hostnames":
@@ -18,7 +18,7 @@ if (isset($_POST['blocklist_id'])) {
     }
     $blocklist = $notracking_url . $file;
 
-    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file $dnsmasq_config", $return);
+    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file $blocklist_dest", $return);
     $jsonData = ['return'=>$return];
     echo json_encode($jsonData);
 }
