1103401fe03db33f5484ad9cb628eb52b311afab
billz <billzimmerman@gmail.com>
2020-03-27 22:05:10 +0000

    replace blocklist dest w/ constant

--- ajax/adblock/update_blocklist.php
+++ ajax/adblock/update_blocklist.php
@@ -6,7 +6,6 @@ require_once '../../includes/config.php';
 if (isset($_POST['blocklist_id'])) {
     $blocklist_id = $_POST['blocklist_id'];
     $notracking_url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/";
-    $blocklist_dest = "/etc/raspap/adblock/";
 
     switch ($blocklist_id) {
         case "notracking-hostnames":
@@ -18,7 +17,7 @@ if (isset($_POST['blocklist_id'])) {
     }
     $blocklist = $notracking_url . $file;
 
-    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file $blocklist_dest", $return);
+    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file " .RASPI_ADBLOCK_LISTPATH, $return);
     $jsonData = ['return'=>$return];
     echo json_encode($jsonData);
 }
