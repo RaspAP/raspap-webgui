0d184323432373aadab3699b941a7d8faf4b27e2
billz <billzimmerman@gmail.com>
2020-03-27 08:46:13 +0000

    Initial commit

---
+++ ajax/adblock/update_blocklist.php
@@ -0,0 +1,25 @@
+<?php
+
+require '../../includes/csrf.php';
+require_once '../../includes/config.php';
+
+if (isset($_POST['blocklist_id'])) {
+    $blocklist_id = $_POST['blocklist_id'];
+    $notracking_url = "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/";
+    $dnsmasq_config = "/etc/dnsmasq.d/";
+
+    switch ($blocklist_id) {
+        case "notracking-hostnames":
+            $file = "hostnames.txt";
+            break;
+        case "notracking-domains":
+            $file = "domains.txt";
+            break;
+    }
+    $blocklist = $notracking_url . $file;
+
+    exec("sudo /etc/raspap/adblock/update_blocklist.sh $blocklist $file $dnsmasq_config", $return);
+     
+    echo json_encode($return);
+}
+
