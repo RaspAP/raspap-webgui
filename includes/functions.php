3fa5215449c6bfeca5bdafc53c80ea4ec1eca6a2
billz <billzimmerman@gmail.com>
2020-04-01 09:09:52 +0000

    Added blocklist last updated

--- includes/functions.php
+++ includes/functions.php
@@ -376,3 +376,44 @@ function optionsForSelect($options)
     }
     return $html;
 }
+
+function blocklistUpdated($file)
+{
+    $blocklist = RASPI_CONFIG.'/adblock/'.$file;
+    if (file_exists($blocklist)) {
+        $lastModified = date ("F d Y H:i:s.", filemtime($blocklist));
+        $lastModified = formatDateAgo($lastModified);
+        return $lastModified;
+    } else {
+        return 'Never';
+    }
+}
+
+function formatDateAgo($datetime, $full = false)
+{
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
--- templates/adblock/general.php
+++ templates/adblock/general.php
@@ -11,21 +11,27 @@
         </div>
         <p id="adblock-description">
           <small><?php echo _("Enable this option if you want RaspAP to <b>block DNS requests for ads, tracking and other virtual garbage</b>. Blocklists are gathered from multiple, actively maintained sources and automatically updated, cleaned, optimized and moderated on a daily basis.") ?></small>
-          <div class="mb-3">
+          <div>
             <small class="text-muted"><?php echo _("This option adds <code>conf-file</code> and <code>addn-hosts</code> to the dnsmasq configuration.") ?></small>
           </div>
         </p>
         </div>
         <div class="row">
-          <div class="input-group col-md-12 mb-4">
-            <select class="custom-select custom-select-sm" id="cbxblocklist" onchange="clearBlocklistStatus()">
-              <option value=""><?php echo _("Choose a blocklist provider") ?></option>
-              <option disabled="disabled"></option>
-              <?php echo optionsForSelect(blocklistProviders()) ?>
-            </select>
-            <div class="input-group-append">
-              <button class="btn btn-sm btn-outline-secondary rounded-right" type="button" onclick="updateBlocklist()"><?php echo _("Update now"); ?></button>
-              <span id="cbxblocklist-status" class="input-group-addon check-hidden ml-2 mt-1"><i class="fas fa-check"></i></span>
+          <div class="col-md-12">
+            <p id="blocklist-updated">
+              <div><small>Hostnames blocklist last updated: <b><?php echo blocklistUpdated('hostnames.txt') ?></b></small></div>
+              <div><small>Domains blocklist last updated: <b><?php echo blocklistUpdated('domains.txt') ?></b></small></div>
+            </p>
+            <div class="input-group col-md-12 mb-4">
+              <select class="custom-select custom-select-sm" id="cbxblocklist" onchange="clearBlocklistStatus()">
+                <option value=""><?php echo _("Choose a blocklist provider") ?></option>
+                <option disabled="disabled"></option>
+                <?php echo optionsForSelect(blocklistProviders()) ?>
+              </select>
+              <div class="input-group-append">
+                <button class="btn btn-sm btn-outline-secondary rounded-right" type="button" onclick="updateBlocklist()"><?php echo _("Update now"); ?></button>
+                <span id="cbxblocklist-status" class="input-group-addon check-hidden ml-2 mt-1"><i class="fas fa-check"></i></span>
+              </div>
             </div>
           </div>
         </div>
