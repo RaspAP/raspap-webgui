b0eb7c63ebe20636152eb9d40db8abd2a952c3ea
billz <billzimmerman@gmail.com>
2020-04-11 16:17:40 +0100

    Update About page

---
+++ templates/about/sponsors.php
@@ -0,0 +1,12 @@
+<!-- about sponsors tab -->
+<div class="tab-pane fade" id="aboutsponsors">
+  <div class="row">
+    <div class="col-md-12 mt-3">
+      <?php 
+      $Parsedown = new Parsedown();
+      $strContent = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/BACKERS.md');
+      echo $Parsedown->text($strContent);
+      ?>
+    </div>
+  </div><!-- /.row -->
+</div><!-- /.tab-pane | sponsors tab -->
