9be1e31daee5ff2a5d41f9c08273e8af878cefad
billz <billzimmerman@gmail.com>
2020-04-13 07:48:53 +0100

    About page tweak

--- templates/about/general.php
+++ templates/about/general.php
@@ -1,7 +1,7 @@
 <!-- about general tab -->
 <div class="tab-pane active" id="aboutgeneral">
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
       <h2 class="mt-3"><?php echo _("RaspAP") . " v" . RASPI_VERSION; ?></h2>
         <div class="ml-5 mt-3"><img class="about-logo" src="app/img/raspAP-logo.png" style="width: 175px; height:175px"></div>
         <div class="mt-3">RaspAP is a co-creation of <a href="https://github.com/billz">billz</a> and <a href="https://github.com/sirlagz">SirLagz</a>
@@ -11,7 +11,7 @@
         <a href="https://github.com/billz/raspap-webgui/wiki/Translations">translator</a> or <a href="https://github.com/billz/raspap-webgui/blob/master/BACKERS.md">financial sponsor</a>.</div>
         <div class="mt-3 project-links">
         <div class="row">
-          <div class="col-md-6">Project home <i class="fab fa-github"></i> <a href="https://github.com/billz/raspap-webgui">raspap-webgui</a></div>
+          <div class="col-md-6">GitHub <i class="fab fa-github"></i> <a href="https://github.com/billz/raspap-webgui">raspap-webgui</a></div>
           <div class="col-md-6">Twitter <span style="color: #55acee"><i class="fab fa-twitter"></i></span> <a href="https://twitter.com/rasp_ap">@RaspAP</a></div>
           <div class="col-md-6">Reddit <span style="color: #ff4500"><i class="fab fa-reddit"></i></span> <a href="https://www.reddit.com/r/RaspAP/">/r/raspap</a></div>
           <div class="col-md-6">License <i class="fas fa-balance-scale"></i> <a href="https://github.com/billz/raspap-webgui/blob/master/LICENSE">GPL-3.0</a></div>
