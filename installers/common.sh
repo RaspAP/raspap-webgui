8d73fb774f46ff3d85253cc7c789638236dabf5a
billz <billzimmerman@gmail.com>
2020-04-22 10:00:34 +0100

    Update installer + sudoers for wg

--- installers/common.sh
+++ installers/common.sh
@@ -235,37 +235,37 @@ function _install_adblock() {
     _install_status 0
 }
 
-# Prompt to install VPN
-function _prompt_install_vpn() {
-    _install_log "Configure VPN support"
-    echo -n "Install VPN and enable client configuration? [Y/n]: "
+# Prompt to install openvpn
+function _prompt_install_openvpn() {
+    _install_log "Configure OpenVPN support"
+    echo -n "Install OpenVPN and enable client configuration? [Y/n]: "
     if [ "$assume_yes" == 0 ]; then
         read answer < /dev/tty
         if [ "$answer" != "${answer#[Nn]}" ]; then
             echo -e
         else
-            _install_vpn
+            _install_openvpn
         fi
     elif [ "$ovpn_option" == 1 ]; then
-        _install_vpn
+        _install_openvpn
     else
         echo "(Skipped)"
     fi
 }
 
-function _install_vpn() {
-    echo -n "Install [O]penVPN or [W]ireGuard? [O/W]: "
+# Prompt to install WireGuard
+function _prompt_install_wireguard() {
+    _install_log "Configure WireGuard support"
+    echo -n "Install WireGuard and enable VPN tunnel configuration? [Y/n]: "
     if [ "$assume_yes" == 0 ]; then
         read answer < /dev/tty
-        case $answer in
-            [oO]* )
-                _install_openvpn;
-                break;;
-            [wW]* )
-                _install_wireguard;
-        esac
-    elif [ "$ovpn_option" == 1 ]; then
-        _install_openvpn
+        if [ "$answer" != "${answer#[Nn]}" ]; then
+            echo -e
+        else
+            _install_wireguard
+        fi
+    elif [ "$wg_option" == 1 ]; then
+        _install_wireguard
     else
         echo "(Skipped)"
     fi
@@ -283,6 +283,8 @@ function _install_wireguard() {
     printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' | sudo tee --append /etc/apt/preferences.d/limit-unstable || _install_status 1 "Unable to append to preferences.d"
     echo "Installing WireGuard"
     sudo apt-get update && sudo apt-get install $apt_option wireguard || _install_status 1 "Unable to install wireguard"
+    echo "Enabling wg-quick@wg0"
+    sudo systemctl enable wg-quick@wg0 || _install_status 1 "Failed to enable wg-quick service"
     echo "Enabling WireGuard management option"
     sudo sed -i "s/\('RASPI_WIREGUARD_ENABLED', \)false/\1true/g" "$webroot_dir/includes/config.php" || _install_status 1 "Unable to modify config.php"
     _install_status 0
@@ -572,7 +574,8 @@ function _install_raspap() {
     _default_configuration
     _configure_networking
     _prompt_install_adblock
-    _prompt_install_vpn
+    _prompt_install_openvpn
+    _prompt_install_wireguard
     _patch_system_files
     _install_complete
 }
