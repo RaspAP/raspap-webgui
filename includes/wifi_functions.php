656ba9234b551a693fd8f3b0b0b9004fd6e71e22
christian Zeitnitz <christian@zeitnitz.eu>
2020-06-09 20:53:11 +0200

    Fix intial assignment of $_SESSION['ap_interface']

--- includes/wifi_functions.php
+++ includes/wifi_functions.php
@@ -144,14 +144,10 @@ function getWifiInterface()
 {
     if (empty($_SESSION['ap_interface'])) {
         $arrHostapdConf = parse_ini_file(RASPI_CONFIG.'/hostapd.ini');
-        if (isset($arrHostapdConf['WifiInterface'])) {
-            $iface = $_SESSION['ap_interface'] = $arrHostapdConf['WifiInterface'];
-            // check for 2nd wifi interface -> wifi client on different interface
-            exec("iw dev | awk '$1==\"Interface\" && $2!=\"$iface\" {print $2}'",$iface2);
-            $_SESSION['wifi_client_interface'] = empty($iface2) ? $iface : trim($iface2[0]);
-        } else { // fallback to default
-            $_SESSION['ap_interface'] = $_SESSION['wifi_client_interface'] = RASPI_WIFI_AP_INTERFACE;
-        }
+        $iface = $_SESSION['ap_interface'] = isset($arrHostapdConf['WifiInterface']) ?  $arrHostapdConf['WifiInterface'] : RASPI_WIFI_AP_INTERFACE;
+        // check for 2nd wifi interface -> wifi client on different interface
+        exec("iw dev | awk '$1==\"Interface\" && $2!=\"$iface\" {print $2}'",$iface2);
+        $_SESSION['wifi_client_interface'] = empty($iface2) ? $iface : trim($iface2[0]);
     }
 }
 
