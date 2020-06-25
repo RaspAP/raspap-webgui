dc790b246b83c10e0ec2d6013cf78a7c2f637990
zbchristian <33725910+zbchristian@users.noreply.github.com>
2020-06-25 16:55:25 +0200

    fix failed copy/paste lines

--- includes/internetRoute.php
+++ includes/internetRoute.php
@@ -5,7 +5,7 @@ define("ACCESS_CHECK_DNS","one.one.one.one");
 
      $rInfo=array();
 // get all default routes
-     exec('ip route list |  sed -rn "s/default via (([0-9]{1,3}\.){3}[0-9]{1,3}).*dev (\w*).*src (([0-9]{1,3}\.){$
+     exec('ip route list |  sed -rn "s/default via (([0-9]{1,3}\.){3}[0-9]{1,3}).*dev (\w*).*src (([0-9]{1,3}\.){3}[0-9]{1,3}).*/\3 \4 \1/p"',$routes);
      if ( !empty($routes) ) {
         foreach ($routes as $i => $route) {
           $prop=explode(' ',$route);
@@ -18,16 +18,16 @@ define("ACCESS_CHECK_DNS","one.one.one.one");
           $rInfo[$i]["gw-name"] = $host[0];
           if (isset($checkAccess) && $checkAccess) {
             // check internet connectivity w/ and w/o DNS resolution
-            exec('ping -W1 -c 1 -I '.$prop[0].' '.ACCESS_CHECK_IP.' |  sed -rn "s/.*icmp_seq=1.*time=.*/\&check\;$
+            exec('ping -W1 -c 1 -I '.$prop[0].' '.ACCESS_CHECK_IP.' |  sed -rn "s/.*icmp_seq=1.*time=.*/\&check\;/p"',$okip);
             if (empty($okip)) $okip[0]="failed";
             $rInfo[$i]["access-ip"] = $okip[0];
-            exec('ping -W1 -c 1 -I '.$prop[0].' '.ACCESS_CHECK_DNS.' |  sed -rn "s/.*icmp_seq=1.*time=.*/\&check\$
+            exec('ping -W1 -c 1 -I '.$prop[0].' '.ACCESS_CHECK_DNS.' |  sed -rn "s/.*icmp_seq=1.*time=.*/\&check\;/p"',$okdns);
             if (empty($okdns)) $okdns[0]="failed";
             $rInfo[$i]["access-dns"] = $okdns[0];
           }
         }
       } else {
-        $data = array("error"=>"No route to the internet found");
+        $rInfo = array("error"=>"No route to the internet found");
       }
       $rInfo_json = json_encode($rInfo);
 ?>
