4d3983bd6e16e8254eaa8bc8263b8b663b3427de
zbchristian <33725910+zbchristian@users.noreply.github.com>
2020-09-10 20:05:00 +0200

    Add packages for mobile data modem

--- installers/common.sh
+++ installers/common.sh
@@ -119,6 +119,7 @@ function _install_dependencies() {
     echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
     sudo apt-get install $apt_option lighttpd git hostapd dnsmasq iptables-persistent $php_package $dhcpcd_package vnstat qrencode || _install_status 1 "Unable to install dependencies"
+    sudo apt-get install wvdial socat bc || _install_status 1 "Unable to install dependencies"
     _install_status 0
 }
 
