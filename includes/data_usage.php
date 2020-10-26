594fb634cce8e50e98234d5455f1b2d3c131f170
brunopiras <55226575+brunopiras@users.noreply.github.com>
2020-10-26 11:05:55 +0100

    Update data_usage.php
    If you've a system with more than 1 or 2 interfaces this mod it shows only activated interfaces. Try, it's my first on GitHub

--- includes/data_usage.php
+++ includes/data_usage.php
@@ -5,7 +5,10 @@
  */
 function DisplayDataUsage(&$extraFooterScripts)
 {
-    exec("ip -o link show | awk -F ': ' '{print $2}' | grep -v lo ", $interfacesWlo);
+    //Original - If you've more than 2 interfaces this show all interfaces (activated and deactivated)
+    //exec("ip -o link show | awk -F ': ' '{print $2}' | grep -v lo ", $interfacesWlo);
+    //Mod - This show only activated interfaces
+    exec("ifconfig | cut -d ' ' -f1 | awk 'NF==1{print $1}'| sed 's/.$//' | grep -v lo", $interfacesWlo);
     echo renderTemplate("data_usage", [ "interfaces" => $interfacesWlo ]);
 
     $extraFooterScripts[] = array('src'=>'dist/datatables/jquery.dataTables.min.js', 'defer'=>false);
