12dd5d824bded89c8187e693a610f64cb73269ab
billz <billzimmerman@gmail.com>
2021-02-07 11:59:02 +0000

    Progress commit: bootstrap modal event handlers

---
+++ ajax/openvpn/activate_ovpncfg.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../../includes/config.php';
+require_once '../../includes/functions.php';
+
+if (isset($_POST['cfg_id'])) {
+    $ovpncfg_id = $_POST['cfg_id'];
+    $ovpncfg_files = pathinfo(RASPI_OPENVPN_CLIENT_LOGIN, PATHINFO_DIRNAME).'/'.$ovpncfg_id.'_*.conf';
+    $return = $ovpncfg_files;
+    //exec("sudo rm $ovpncfg_files", $return);
+    $jsonData = ['return'=>$return];
+    echo json_encode($jsonData);
+}
+
