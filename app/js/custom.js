96bd34f07fe5dcc3b4107f4dd7834bc23591f160
billz <billzimmerman@gmail.com>
2021-03-07 13:19:56 +0000

    Add event listener for Bootstrap form validation

--- app/js/custom.js
+++ app/js/custom.js
@@ -361,6 +361,22 @@ $('.wg-keygen').click(function(){
     })
 })
 
+// Event listener for Bootstrap's form validation
+window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
+
 // Static Array method
 Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 
