e5ac7d23a3ece96af5a4bd4dafcd34e81f69db7e
glaszig <glaszig@gmail.com>
2021-04-03 17:49:04 +0100

    improve table layout 'internet connection'

--- templates/networking.php
+++ templates/networking.php
@@ -18,40 +18,38 @@
             <h4 class="mt-3"><?php echo _("Internet connection"); ?></h4>
             <div class="row">
              <div class="col-sm-12">
-              <div class="card ">
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table table-hover">
-                      <thead>
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th><?php echo _("Interface"); ?></th>
+                      <th><?php echo _("IP Address"); ?></th>
+                      <th><?php echo _("Gateway"); ?></th>
+                      <th colspan="2"><?php echo _("Internet Access"); ?></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php if (isset($routeInfo["error"]) || empty($routeInfo)): ?>
+                    <tr><td colspan=5>No route to the internet found</td></tr>
+                    <?php else: ?>
+                      <?php foreach($routeInfo as $route): ?>
                         <tr>
-                          <th><?php echo _("Interface"); ?></th>
-                          <th><?php echo _("IP Address"); ?></th>
-                          <th><?php echo _("Gateway"); ?></th>
-                          <th colspan="2"><?php echo _("Internet Access"); ?></th>
+                          <td><?php echo $route['interface'] ?></td>
+                          <td><?php echo $route['ip-address'] ?></td>
+                          <td><?php echo $route['gateway'] ?><br><?php $route['gw-name'] ?></td>
+                          <td>
+                            <p class="m-0">
+                              <i class="fas <?php echo $route["access-ip"] ? "fa-check" : "fa-times"; ?>"></i> <?php echo RASPI_ACCESS_CHECK_IP ?>
+                            </p>
+                            <p class="m-0">
+                              <i class="fas <?php echo $route["access-dns"] ? "fa-check" : "fa-times"; ?>"></i> <?php echo RASPI_ACCESS_CHECK_DNS ?>
+                            </p>
+                          </td>
                         </tr>
-                      </thead>
-                      <tbody>
-                        <?php
-                        if (isset($routeInfo["error"]) || empty($routeInfo)) {
-                            echo "<tr><td colspan=5>No route to the internet found</td></tr>";
-                        } else {
-                            foreach($routeInfo as $route) {
-                                echo "<tr>";
-                                echo "<td>".$route["interface"]."</td>";
-                                echo "<td>".$route["ip-address"]."</td>";
-                                echo "<td>".$route["gateway"]."<br>".$route["gw-name"]."</td>";
-                                $status = $route["access-ip"] ? "fa-check" : "fa-times";
-                                echo '<td><i class="fas '.$status.'"></i><br>'.RASPI_ACCESS_CHECK_IP.'</td>';
-                                $status = $route["access-dns"] ? "fa-check" : "fa-times";
-                                echo '<td><i class="fas '.$status.'"></i><br>'.RASPI_ACCESS_CHECK_DNS.'</td>';
-                                echo "</tr>";
-                            }
-                        }
-                        ?>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+                      <?php endforeach ?>
+                    <?php endif ?>
+                  </tbody>
+                </table>
               </div>
              </div>
             </div>
