d3c830d69f6b4bfd3b3713f72667db1e500cbd66
billz <billzimmerman@gmail.com>
2021-04-06 10:34:39 +0100

    Wrap strings w/ php gettext, update en_US locale + compile

--- templates/openvpn/general.php
+++ templates/openvpn/general.php
@@ -11,17 +11,17 @@
       <h5><?php echo _("Authentification Method"); ?></h5>
       <div class="col-sm-12 mt-2 mb-2 form-check">
           <input class="form-check-input" id="ovpn-userpw" name="sel1" value="userpw" data-toggle="" data-parent="#clientsettings" data-target="#UserPW" type="radio" checked>
-          <label class="form-check-label">User name and password</label>
+          <label class="form-check-label"><?php echo _("Username and password"); ?></label>
       </div>
       <div class="col-sm-12 mt-2 mb-2 form-check">
           <input class="form-check-input" id="ovpn-certs" name="sel1" value="certs" data-toggle="" data-parent="#clientsettings" data-target="#Certs" type="radio">
-          <label class="form-check-label">Certificates</label>
+          <label class="form-check-label"><?php echo _("Certificates"); ?></label>
       </div>
       <div class="col-sm-12 ml-2">
          <div class="panel-group" id="clientsettings">
            <div class="panel panel-default panel-collapse" id="PanelUserPW" >
              <div class="panel-heading">
-               <h5 class="panel-title">Enter username and password</h5>
+               <h5 class="panel-title"><?php echo _("Enter username and password"); ?></h5>
              </div>
              <div class="panel-body">
                 <div class="form-group col-lg-12">
@@ -33,26 +33,28 @@
                   <input type="password" class="form-control" name="authPassword" value="<?php echo htmlspecialchars($authPassword, ENT_QUOTES); ?>" />
                 </div>
              </div>
-           </div> <!-- panel -->
-           <div class="panel panel-default panel-collapse collapse in" id="PanelCerts" >
-              <div class="panel-body">
-             <div class="panel-heading">
-               <h5 class="panel-title">Certificates in the configuration file</h5>
-             </div>
-                    <p> RaspAP does not support the import of the required cerficates. Please paste them into the configuration file
-                      <ul>
-                        <li>Signing certification authority (CA) certificate (e.g. <code>ca.crt</code>): enclosed in <code>&lt;ca> ... &lt;/ca></code> tags </li>
-                        <li>Client certificate (public key) (e.g. <code>client.crt</code>): enclosed in <code>&lt;cert> ... &lt;/cert></code> tags </li>
-                        <li>Private key of the client certificate (e.g. <code>client.key</code>): enclosed in <code>&lt;key> ... &lt;/key></code> tags </li>
-                      </ul>
-                    </p>
+           </div><!-- panel -->
+           <div class="panel panel-default panel-collapse collapse in" id="PanelCerts">
+             <div class="panel-body">
+               <div class="panel-heading">
+                 <h5 class="panel-title"><?php echo _("Certificates in the configuration file"); ?></h5>
+               </div>
+               <p><?php echo _("RaspAP supports certificates by including them in the configuration file."); ?>
+                 <ul>
+                   <small>
+                     <li><?php echo _("Signing certification authority (CA) certificate (e.g. <code>ca.crt</code>): enclosed in <code>&lt;ca> ... &lt;/ca></code> tags."); ?></li>
+                     <li><?php echo _("Client certificate (public key) (e.g. <code>client.crt</code>): enclosed in <code>&lt;cert> ... &lt;/cert></code> tags."); ?></li>
+                     <li><?php echo _("Private key of the client certificate (e.g. <code>client.key</code>): enclosed in <code>&lt;key> ... &lt;/key></code> tags."); ?></li>
+                   </small>
+                 </ul>
+                </p>
               </div>
            </div> <!-- panel -->
          </div> <!-- panel-group -->
       </div> <!-- col -->
       <div class="col-sm-12 ">
          <div class="form-group">
-            <h5 class="panel-title">Configuration File</h4>
+            <h5 class="panel-title"><?php echo _("Configuration File"); ?></h4>
             <div class="custom-file">
              <input type="file" class="custom-file-input" name="customFile" id="customFile">
              <label class="custom-file-label" for="customFile"><?php echo _("Select OpenVPN configuration file (.ovpn)"); ?></label>
