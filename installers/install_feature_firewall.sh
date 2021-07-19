5197df18e14bc0600fb9822e84f4fd71f7be66c9
Christian Zeitnitz <zeitnitz@uni-wuppertal.de>
2021-07-19 17:47:38 +0200

    Fix ownership of firewall directory

--- installers/install_feature_firewall.sh
+++ installers/install_feature_firewall.sh
@@ -11,10 +11,10 @@ function _install_feature_firewall() {
     name="feature firewall"
 
     _install_log "Install $name"
-    _install_log " - copy configuration file"
 	# create config dir
-	sudo mkdir "/etc/raspap/networking/firewall" || _install_status 1 "Unable to create firewall config directory 
+	sudo mkdir "$raspap_network/firewall" || _install_status 1 "Unable to create firewall config directory" 
     # copy firewall configuration 
-    sudo cp "$webroot_dir/config/iptables_rules.json" "/etc/raspap/networking/" || _install_status 1 "Unable to install client configuration ($name)"
+    sudo cp "$webroot_dir/config/iptables_rules.json" "$raspap_network/firewall/" || _install_status 1 "Unable to copy iptables templates"
+	sudo chown $raspap_user:$raspap_user -R "$raspap_network/firewall" || _install_status 1 "Unable to change ownership of firewall directory and files "
     _install_status 0
 }
