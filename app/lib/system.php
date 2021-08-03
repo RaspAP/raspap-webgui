c3a219e34077436c9008d7475d8b8b868fa449c8
billz <billzimmerman@gmail.com>
2021-08-03 15:20:08 +0100

    Update class w/ namespace, phpcbf formatting

--- app/lib/system.php
+++ app/lib/system.php
@@ -1,11 +1,24 @@
 <?php
 
-class System {
-    public function hostname() {
+/**
+ * Sytem info class
+ *
+ * @description System info class for RaspAP
+ * @author      Bill Zimmerman <billzimmerman@gmail.com>
+ * @license     https://github.com/raspap/raspap-webgui/blob/master/LICENSE
+ */
+
+namespace RaspAP\System;
+
+class Sysinfo
+{
+    public function hostname()
+    {
         return shell_exec("hostname -f");
     }
 
-    public function uptime() {
+    public function uptime()
+    {
         $uparray = explode(" ", exec("cat /proc/uptime"));
         $seconds = round($uparray[0], 0);
         $minutes = $seconds / 60;
@@ -15,10 +28,10 @@ class System {
         $minutes = floor($minutes - ($days * 24 * 60) - ($hours * 60));
         $uptime= '';
         if ($days    != 0) {
-            $uptime .= $days    . ' day'    . (($days    > 1)? 's ':' ');
+            $uptime .= $days . ' day' . (($days    > 1)? 's ':' ');
         }
         if ($hours   != 0) {
-            $uptime .= $hours   . ' hour'   . (($hours   > 1)? 's ':' ');
+            $uptime .= $hours . ' hour' . (($hours   > 1)? 's ':' ');
         }
         if ($minutes != 0) {
             $uptime .= $minutes . ' minute' . (($minutes > 1)? 's ':' ');
@@ -27,33 +40,40 @@ class System {
         return $uptime;
     }
 
-    public function usedMemory() {
+    public function usedMemory()
+    {
         $used = shell_exec("free -m | awk '/Mem:/ { total=$2 ; used=$3 } END { print used/total*100}'");
         return floor($used);
     }
 
-    public function processorCount() {
+    public function processorCount()
+    {
         $procs = shell_exec("nproc --all");
         return intval($procs);
     }
 
-    public function loadAvg1Min() {
+    public function loadAvg1Min()
+    {
         $load = exec("awk '{print $1}' /proc/loadavg");
         return floatval($load);
     }
 
-    public function systemLoadPercentage() {
+    public function systemLoadPercentage()
+    {
         return intval(($this->loadAvg1Min() * 100) / $this->processorCount());
     }
 
-    public function systemTemperature() {
+    public function systemTemperature()
+    {
         $cpuTemp = file_get_contents("/sys/class/thermal/thermal_zone0/temp");
         return number_format((float)$cpuTemp/1000, 1);
     }
 
-    public function hostapdStatus() {
+    public function hostapdStatus()
+    {
         exec('pidof hostapd | wc -l', $status);
         return $status;
     }
 
 }
+
