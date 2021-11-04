84bd1410259bf1e6d02dc950dc327fe470cf96ca
billz <billzimmerman@gmail.com>
2021-11-04 09:50:21 +0000

    Update release version

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
  * @author  Lawrence Yau <sirlagz@gmail.com>
  * @author  Bill Zimmerman <billzimmerman@gmail.com>
  * @license GNU General Public License, version 3 (GPL-3.0)
- * @version 2.7.5
+ * @version 2.7.6
  * @link    https://github.com/raspap/raspap-insiders/
  * @link    https://raspap.com/
  * @see     http://sirlagz.net/2013/02/08/raspap-webgui/
