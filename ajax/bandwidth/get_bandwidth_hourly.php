4be05fe85ede229b73e64cce23e3f3ea85eb675d
billz <billzimmerman@gmail.com>
2021-11-21 18:45:01 +0000

    Fix for vnStat v2.6 (bullseye) breaking changes. Resolves #1064

--- ajax/bandwidth/get_bandwidth_hourly.php
+++ ajax/bandwidth/get_bandwidth_hourly.php
@@ -42,8 +42,8 @@ if (filter_input(INPUT_GET, 'tu') == 'h') {
     $jsonobj = json_decode($jsonstdoutvnstat[0], true)['interfaces'][0];
     $jsonData = $jsonobj['traffic']['hour'];
     for ($i = count($jsonData) - 1; $i >= 0; --$i) {
-        $data_template[$jsonData[$i]['id']]['rx'] = round($jsonData[$i]['rx'] / 1024, 0);
-        $data_template[$jsonData[$i]['id']]['tx'] = round($jsonData[$i]['tx'] / 1024, 0);
+        $data_template[$jsonData[$i]['time']['hour']]['rx'] = round($jsonData[$i]['rx'] / 1024, 0);
+        $data_template[$jsonData[$i]['time']['hour']]['tx'] = round($jsonData[$i]['tx'] / 1024, 0);
     }
 
     $data = array();
