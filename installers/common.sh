da38b48a6c95b01fecb66c45ee249399742d7f7a
Steven Foerster <sfoerster@gmail.com>
2022-01-30 20:45:52 -0500

    removing stale iptables rule in installer

--- installers/common.sh
+++ installers/common.sh
@@ -554,7 +554,6 @@ function _configure_networking() {
     echo "Checking iptables rules"
     rules=(
     "-A POSTROUTING -j MASQUERADE"
-    "-A POSTROUTING -s 192.168.50.0/24 ! -d 192.168.50.0/24 -j MASQUERADE"
     )
     for rule in "${rules[@]}"; do
         if grep -- "$rule" $rulesv4 > /dev/null; then
