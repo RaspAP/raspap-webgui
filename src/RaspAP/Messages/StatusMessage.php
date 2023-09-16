07b950cf659146ab6864190c5ad595fbcdaecd25
billz <billzimmerman@gmail.com>
2023-09-16 11:43:40 +0200

    Migrate includes/status_message.php to RaspAP/Messages

---
+++ src/RaspAP/Messages/StatusMessage.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Status message class
+ *
+ * @description Status message class for RaspAP
+ * @author      Bill Zimmerman <billzimmerman@gmail.com>
+ * @license     https://github.com/raspap/raspap-webgui/blob/master/LICENSE
+ */
+
+namespace RaspAP\Messages;
+
+class StatusMessage
+{
+    public $messages = array();
+
+    public function addMessage($message, $level = 'success', $dismissable = true)
+    {
+        $status = '<div class="alert alert-'.$level;
+        if ($dismissable) {
+            $status .= ' alert-dismissable';
+        }
+        $status .= '">'. _($message);
+        if ($dismissable) {
+            $status .= '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>';
+        }
+        $status .= '</div>';
+
+        array_push($this->messages, $status);
+    }
+
+    public function showMessages($clear = true)
+    {
+        foreach ($this->messages as $message) {
+            echo $message;
+        }
+        if ($clear) {
+            $this->messages = array();
+        }
+    }
+}
