d619269df4b21f20aedd5c4b6d91c7aa902808b8
billz <billzimmerman@gmail.com>
2023-10-31 15:17:13 +0000

    CSRF check, exec system/debuglog.sh

--- ajax/system/sys_debug.php
+++ ajax/system/sys_debug.php
@@ -3,8 +3,13 @@
 require '../../includes/csrf.php';
 require_once '../../includes/config.php';
 
+if (isset($_POST['csrf_token'])) {
+    if (csrfValidateRequest() && !CSRFValidate()) {
+        handleInvalidCSRFToken();
+    }
+    exec( RASPI_CONFIG.'/system/debuglog.sh', $return);
+    echo json_encode(end($return));
+} else {
+    handleInvalidCSRFToken();
+}
 
-$return = 0;
-echo json_encode($return);
-
-?>
