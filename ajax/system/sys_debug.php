22cd0ae017c1390f9b09abf3ef995bff60deef0d
billz <billzimmerman@gmail.com>
2023-11-01 07:46:36 +0000

    Add system/debuglog.sh to sudoers

--- ajax/system/sys_debug.php
+++ ajax/system/sys_debug.php
@@ -8,7 +8,7 @@ if (isset($_POST['csrf_token'])) {
         handleInvalidCSRFToken();
     }
     $root = getenv("DOCUMENT_ROOT");
-    exec( RASPI_CONFIG.'/system/debuglog.sh -i '.$root, $return);
+    exec('sudo '.RASPI_CONFIG.'/system/debuglog.sh -i '.$root, $return);
 
     $logOutput = implode(PHP_EOL, $return);
     $tempDir = sys_get_temp_dir();
