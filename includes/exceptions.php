adf44d462be76fdd347d5ec6a245b9fdd9f5dd1b
billz <billzimmerman@gmail.com>
2023-12-27 05:35:03 +0000

    Move set_exception_handler to constructor

--- includes/exceptions.php
+++ includes/exceptions.php
@@ -1,10 +1,6 @@
 <?php
 require_once 'src/RaspAP/Exceptions/ExceptionHandler.php';
 
-// Import the ExceptionHandler class
-use RaspAP\Exceptions\ExceptionHandler;
-
-set_exception_handler(['RaspAP\Exceptions\ExceptionHandler', 'handleException']);
-register_shutdown_function(['RaspAP\Exceptions\ExceptionHandler', 'handleFatalError']);
+$handler = new RaspAP\Exceptions\ExceptionHandler;
 ?>
 
