20bc1830b4898a23768168413a9c48e9824469bd
billz <billzimmerman@gmail.com>
2024-02-08 10:26:43 +0100

    Update release version

--- includes/defaults.php
+++ includes/defaults.php
@@ -6,7 +6,7 @@ if (!defined('RASPI_CONFIG')) {
 
 $defaults = [
   'RASPI_BRAND_TEXT' => 'RaspAP',
-  'RASPI_VERSION' => '3.0.7',
+  'RASPI_VERSION' => '3.0.8',
   'RASPI_CONFIG_NETWORK' => RASPI_CONFIG.'/networking/defaults.json',
   'RASPI_CONFIG_PROVIDERS' => 'config/vpn-providers.json',
   'RASPI_ADMIN_DETAILS' => RASPI_CONFIG.'/raspap.auth',
