4bf514c992160dcdb5b8c275568bb24fe2b27e85
billz <billzimmerman@gmail.com>
2024-02-27 11:49:21 +0100

    Add required php-mbstring to _install_dependencies

--- installers/common.sh
+++ installers/common.sh
@@ -250,7 +250,7 @@ function _install_dependencies() {
     # Set dconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
     echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-    sudo apt-get install -y lighttpd git hostapd dnsmasq iptables-persistent $php_package $dhcpcd_package $iw_package $rsync_package $network_tools vnstat qrencode jq isoquery || _install_status 1 "Unable to install dependencies"
+    sudo apt-get install -y lighttpd git hostapd dnsmasq iptables-persistent $php_package $dhcpcd_package $iw_package $rsync_package $network_tools vnstat qrencode jq isoquery php-mbstring || _install_status 1 "Unable to install dependencies"
     _install_status 0
 }
 
