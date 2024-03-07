75be1bf04e3a772e28f313857f0cc6f216d585a2
billz <billzimmerman@gmail.com>
2024-03-07 18:53:17 +0100

    Add load_dotenv from dotenv

--- api/auth.py
+++ api/auth.py
@@ -2,6 +2,9 @@ import os
 from fastapi.security.api_key import APIKeyHeader
 from fastapi import Security, HTTPException
 from starlette.status import HTTP_403_FORBIDDEN
+from dotenv import load_dotenv
+
+load_dotenv()
 
 apikey=os.getenv('RASPAP_API_KEY')
 #if env not set, set the api key to "insecure"
@@ -18,3 +21,4 @@ async def get_api_key(api_key_header: str = Security(api_key_header)):
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="403: Unauthorized"
         )
+
