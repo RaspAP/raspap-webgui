ce72d2a0abe699c5e06046275b7e3d97408c7761
billz <billzimmerman@gmail.com>
2024-11-07 10:23:34 -0800

    Update interface with renderTemplate() method

--- src/RaspAP/Plugins/PluginInterface.php
+++ src/RaspAP/Plugins/PluginInterface.php
@@ -22,5 +22,13 @@ interface PluginInterface
      * @param Sidebar $sidebar Sidebar instance for adding items
      */
     public function initialize(Sidebar $sidebar): void;
+
+    /**
+     * Render a template within the plugin's template directory
+     * @param string $templateName
+     * @param array $__data
+     * @return string
+     */
+    public function renderTemplate(string $templateName, array $__data = []): string;
 }
 
