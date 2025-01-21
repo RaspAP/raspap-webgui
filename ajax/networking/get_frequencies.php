bc7d4ef1c17083fa9d5911a41f57a1479adce68e
billz <billzimmerman@gmail.com>
2025-01-21 04:59:57 -0800

    Include session in ajax handlers

--- ajax/networking/get_frequencies.php
+++ ajax/networking/get_frequencies.php
@@ -1,6 +1,7 @@
 <?php
 
 require '../../includes/csrf.php';
+require_once '../../includes/session.php';
 require_once '../../includes/config.php';
 require_once '../../src/RaspAP/Auth/HTTPAuth.php';
 require_once '../../src/RaspAP/Parsers/IwParser.php';
