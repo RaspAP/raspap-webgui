c235a4ff1674e17079e0e8ef2c9ccf92c18ab251
billz <billzimmerman@gmail.com>
2025-03-23 01:21:28 -0700

    Create zero.php, move under /devices

--- app/img/devices/default.php
+++ app/img/devices/default.php
@@ -1,6 +1,6 @@
 <?php header("Content-Type: image/svg+xml; charset=utf-8"); ?>
 <?php
-require_once '../../includes/functions.php';
+require_once '../../../includes/functions.php';
 $color = getColorOpt();
 ?>
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
