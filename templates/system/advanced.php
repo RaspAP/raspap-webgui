4f90c035ca38057b539e301539b91d11eba61dc7
billz <billzimmerman@gmail.com>
2025-07-25 21:10:20 -0700

    Move alert message options to theme tab

--- templates/system/advanced.php
+++ templates/system/advanced.php
@@ -22,22 +22,6 @@
           <input type="text" class="form-control" name="logLimit" value="<?php echo htmlspecialchars($logLimit, ENT_QUOTES); ?>" />
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-6 mb-2">
-          <div class="form-check form-switch">
-            <?php $checked = $optAutoclose == 1 ? 'checked="checked"' : '' ?>
-            <input class="form-check-input" id="chxautoclose" name="autoClose" type="checkbox" value="1" <?php echo $checked ?> />
-            <label class="form-check-label" for="chxautoclose"><?php echo _("Automatically close alerts after a specified timeout"); ?></label>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="mb-3 col-md-6">
-          <label for="code"><?php echo _("Alert close timeout (milliseconds)") ;?></label>
-          <input type="text" class="form-control" name="alertTimeout" value="<?php echo htmlspecialchars($alertTimeout, ENT_QUOTES); ?>" />
-        </div>
-      </div>
-
       <input type="submit" class="btn btn-outline btn-primary" name="SaveServerSettings" value="<?php echo _("Save settings"); ?>" />
       <input type="submit" class="btn btn-warning" name="RestartLighttpd" value="<?php echo _("Restart lighttpd"); ?>" />
     </form>
