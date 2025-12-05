7cb3e6285078d09cb5e5d5888a6e4e5d24774da6
ShH Y <74806550+1208nn@users.noreply.github.com>
2025-12-05 23:06:16 +0800

    Use sudo for grep in common.sh

--- installers/common.sh
+++ installers/common.sh
@@ -836,7 +836,7 @@ function _configure_networking() {
     "-A POSTROUTING -s 192.168.50.0/24 ! -d 192.168.50.0/24 -j MASQUERADE"
     )
     for rule in "${rules[@]}"; do
-        if grep -- "$rule" $rulesv4 > /dev/null; then
+        if sudo grep -- "$rule" $rulesv4 > /dev/null; then
             echo "Rule already exits: ${rule}"
         else
             rule=$(sed -e 's/^\(-A POSTROUTING\)/-t nat \1/' <<< $rule)
