2d9ad6da77cf071077b98d7bba3ce10393e4d7d4
Shawn Holmes <15840900+Jixabon@users.noreply.github.com>
2026-02-21 22:21:13 -0700

    add space for better readability

--- templates/adblock.php
+++ templates/adblock.php
@@ -7,6 +7,7 @@
 <div class="row">
   <div class="col-lg-12">
     <div class="card">
+      
       <div class="card-header page-card-header">
         <div class="d-flex justify-content-between align-items-center">
           <div>
