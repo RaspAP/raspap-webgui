572681ceb8dcac241f7249ecc973277b0e0c8334
Shawn Holmes <15840900+Jixabon@users.noreply.github.com>
2026-02-21 22:21:25 -0700

    move openvpn service controls

--- templates/openvpn.php
+++ templates/openvpn.php
@@ -7,7 +7,6 @@
 <div class="row">
   <div class="col-lg-12">
     <div class="card">
-
       <div class="card-header page-card-header">
         <div class="d-flex justify-content-between align-items-center">
           <div>
