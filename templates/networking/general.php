0cc859100c791b282dfafee8f1a923dde274135b
Shawn Holmes <15840900+Jixabon@users.noreply.github.com>
2026-04-18 10:48:36 -0600

    indentation fix

--- templates/networking/general.php
+++ templates/networking/general.php
@@ -1,102 +1,101 @@
-  <div role="tabpanel" class="tab-pane fade show active" id="summary">
-    <div class="d-flex justify-content-between align-items-center mt-3">
-      <h4><?php echo _("Internet connection"); ?></h4>
-      <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh") ?></button>
+<div role="tabpanel" class="tab-pane fade show active" id="summary">
+  <div class="d-flex justify-content-between align-items-center mt-3">
+    <h4><?php echo _("Internet connection"); ?></h4>
+    <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-outline btn-primary"><i class="fas fa-sync-alt"></i> <?php echo _("Refresh") ?></button>
+  </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th><?php echo _("Interface"); ?></th>
+              <th><?php echo _("IP Address"); ?></th>
+              <th><?php echo _("Gateway"); ?></th>
+              <th colspan="2"><?php echo _("Internet Access"); ?></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php if (isset($routeInfo["error"]) || empty($routeInfo)): ?>
+            <tr><td colspan=5>No route to the internet found</td></tr>
+            <?php else: ?>
+              <?php foreach($routeInfo as $route): ?>
+                <tr>
+                  <td><?php echo $route['interface'] ?></td>
+                  <td><?php echo $route['ip-address'] ?></td>
+                  <td><?php echo $route['gateway'] ?><br><?php $route['gw-name'] ?></td>
+                  <?php if ($route['isAP']): ?>
+                    <td>
+                      <p class="m-0">Access point</p>
+                    </td>
+                  <?php else: ?>
+                    <td>
+                      <p class="m-0"  data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo "ping ".RASPI_ACCESS_CHECK_IP ?>">
+                        <i class="fas <?php echo $route["access-ip"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "IP" ?>
+                      </p>
+                      <p class="m-0" data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo "ping ".RASPI_ACCESS_CHECK_DNS ?>">
+                        <i class="fas <?php echo $route["access-dns"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "DNS" ?>
+                      </p>
+                      <p class="m-0" data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo RASPI_ACCESS_CHECK_URL ?>">
+                        <i class="fas <?php echo $route["access-url"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "HTTP" ?>
+                      </p>
+                    </td>
+                  <?php endif ?>
+                </tr>
+              <?php endforeach ?>
+            <?php endif ?>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="table-responsive">
-          <table class="table">
-            <thead>
-              <tr>
-                <th><?php echo _("Interface"); ?></th>
-                <th><?php echo _("IP Address"); ?></th>
-                <th><?php echo _("Gateway"); ?></th>
-                <th colspan="2"><?php echo _("Internet Access"); ?></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php if (isset($routeInfo["error"]) || empty($routeInfo)): ?>
-              <tr><td colspan=5>No route to the internet found</td></tr>
-              <?php else: ?>
-                <?php foreach($routeInfo as $route): ?>
+  </div>
+
+  <h4 class="mt-3"><?php echo _("Routing table"); ?></h4>
+  <div class="card h-100 w-100">
+    <div class="card-header">
+      <div class="d-flex justify-content-between align-items-center">
+        <span><?php echo _("raw output") ?></span>
+        <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-primary"><i class="fas fa-sync-alt"></i></button>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="table-responsive">
+            <table class="table">
+              <tbody>
+                <?php foreach($routeInfoRaw as $route): ?>
                   <tr>
-                    <td><?php echo $route['interface'] ?></td>
-                    <td><?php echo $route['ip-address'] ?></td>
-                    <td><?php echo $route['gateway'] ?><br><?php $route['gw-name'] ?></td>
-                    <?php if ($route['isAP']): ?>
-                      <td>
-                        <p class="m-0">Access point</p>
-                      </td>
-                    <?php else: ?>
-                      <td>
-                        <p class="m-0"  data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo "ping ".RASPI_ACCESS_CHECK_IP ?>">
-                          <i class="fas <?php echo $route["access-ip"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "IP" ?>
-                        </p>
-                        <p class="m-0" data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo "ping ".RASPI_ACCESS_CHECK_DNS ?>">
-                          <i class="fas <?php echo $route["access-dns"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "DNS" ?>
-                        </p>
-                        <p class="m-0" data-bs-toggle="tooltip" data-bs-placement="left" title="<?php echo RASPI_ACCESS_CHECK_URL ?>">
-                          <i class="fas <?php echo $route["access-url"] ? "fa-check" : "fa-times"; ?>" ></i> <?php echo "HTTP" ?>
-                        </p>
-                      </td>
-                    <?php endif ?>
+                    <pre class="unstyled"><?php echo $route; ?></pre>
                   </tr>
                 <?php endforeach ?>
-              <?php endif ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-
-    <h4 class="mt-3"><?php echo _("Routing table"); ?></h4>
-    <div class="card h-100 w-100">
-      <div class="card-header">
-        <div class="d-flex justify-content-between align-items-center">
-          <span><?php echo _("raw output") ?></span>
-          <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-primary"><i class="fas fa-sync-alt"></i></button>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="table-responsive">
-              <table class="table">
-                <tbody>
-                  <?php foreach($routeInfoRaw as $route): ?>
-                    <tr>
-                      <pre class="unstyled"><?php echo $route; ?></pre>
-                    </tr>
-                  <?php endforeach ?>
-                </tbody>
-              </table>
-            </div>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <h4 class="mt-3"><?php echo _("Current settings") ?></h4>
-    <div class="row">
-      <?php if (!$bridgedEnabled) : // No interface details when bridged ?>
-        <?php foreach ($interfaces as $interface): ?>
-          <?php $if_quoted = htmlspecialchars($interface, ENT_QUOTES) ?>
-          <div class="col-md mb-3">
-            <div class="card h-100 w-100">
-              <div class="card-header">
-                <div class="d-flex justify-content-between align-items-center">
-                  <span><?php echo $if_quoted ?></span>
-                  <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-primary"><i class="fas fa-sync-alt"></i></button>
-                </div>
-              </div>
-              <div class="card-body">
-                <pre class="unstyled" id="<?php echo $if_quoted ?>-summary"></pre>
+  <h4 class="mt-3"><?php echo _("Current settings") ?></h4>
+  <div class="row">
+    <?php if (!$bridgedEnabled) : // No interface details when bridged ?>
+      <?php foreach ($interfaces as $interface): ?>
+        <?php $if_quoted = htmlspecialchars($interface, ENT_QUOTES) ?>
+        <div class="col-md mb-3">
+          <div class="card h-100 w-100">
+            <div class="card-header">
+              <div class="d-flex justify-content-between align-items-center">
+                <span><?php echo $if_quoted ?></span>
+                <button type="button" onClick="window.location.reload();" class="btn btn-sm btn-primary"><i class="fas fa-sync-alt"></i></button>
               </div>
             </div>
+            <div class="card-body">
+              <pre class="unstyled" id="<?php echo $if_quoted ?>-summary"></pre>
+            </div>
           </div>
-        <?php endforeach ?>
-      <?php endif ?>
-    </div><!-- /.row -->
-  </div><!-- /.tabpanel -->
-
+        </div>
+      <?php endforeach ?>
+    <?php endif ?>
+  </div><!-- /.row -->
+</div><!-- /.tabpanel -->
