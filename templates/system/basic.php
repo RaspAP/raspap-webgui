8bee39c342b2b1d440a34c50bc2a9d5e37db757d
Shawn Holmes <15840900+Jixabon@users.noreply.github.com>
2026-04-18 22:20:04 -0600

    change layout of info

--- templates/system/basic.php
+++ templates/system/basic.php
@@ -32,23 +32,29 @@ include('includes/sysstats.php');
       </div>
 
       <div class="d-flex flex-column mb-3">
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("Hostname"); ?></div><div class="info-value col"><?php echo htmlspecialchars($hostname, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("Hostname"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($hostname, ENT_QUOTES); ?></div>
         </div>
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("Pi Revision"); ?></div><div class="info-value col"><?php echo htmlspecialchars($revision, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("Pi Revision"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($revision, ENT_QUOTES); ?></div>
         </div>
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("OS"); ?></div><div class="info-value col"><?php echo htmlspecialchars($os, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("OS"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($os, ENT_QUOTES); ?></div>
         </div>
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("Kernel"); ?></div><div class="info-value col"><?php echo htmlspecialchars($kernel, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("Kernel"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($kernel, ENT_QUOTES); ?></div>
         </div>
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("Uptime"); ?></div><div class="info-value col"><?php echo htmlspecialchars($uptime, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("Uptime"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($uptime, ENT_QUOTES); ?></div>
         </div>
-        <div class="row mb-1">
-          <div class="info-item col-4"><?php echo _("System Time"); ?></div><div class="info-value col"><?php echo htmlspecialchars($systime, ENT_QUOTES); ?></div>
+        <div class="d-flex mb-1">
+          <div class="info-item"><?php echo _("System Time"); ?></div>
+          <div class="info-value"><?php echo htmlspecialchars($systime, ENT_QUOTES); ?></div>
         </div>
       </div>
 
